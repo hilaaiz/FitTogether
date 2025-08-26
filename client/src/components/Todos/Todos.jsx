3cb8a87deacb1aa1ed83e9d88ede31efa5f6442f
shani <shanileviw@gmail.com>
2025-08-26 21:29:05 +0300

    challenges directory + run client

---
+++ client/src/components/Todos/Todos.jsx
@@ -0,0 +1,9 @@
+
+export default function UserInfo() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>User Info Page</h1>
+      <p>Placeholder component – content coming soon 🚧</p>
+    </div>
+  );
+}
